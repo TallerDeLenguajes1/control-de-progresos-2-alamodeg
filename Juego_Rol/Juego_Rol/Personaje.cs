0dfb1c8e209860d3a6ace5b79f67b0eb42479f22
Alvaro <alvarodeguer@gmail.com>
2020-06-02 16:21:14 -0300

    Triste intento del juego de rol (me fui afk 10 min)

---
+++ Juego_Rol/Juego_Rol/Personaje.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Juego_Rol
+{
+    class Personaje
+    {
+		enum TRAZA { Orco = 1, Humano = 2, Loli = 3 };
+		string[] nombres = { "Destructor", "Demoledor" };
+
+		
+		//datos
+		
+		int velocidad;
+		int destreza;
+		int fuerza;
+		int nivel;
+		int armadura;
+		
+		//caracteristicas
+		
+		TRAZA raza;
+		string nombre;
+		DateTime fechanacimiento;
+		int edad;
+		int salud;
+
+        public int Velocidad { get => velocidad; set => velocidad = value; }
+        public int Destreza { get => destreza; set => destreza = value; }
+        public int Fuerza { get => fuerza; set => fuerza = value; }
+        public int Nivel { get => nivel; set => nivel = value; }
+        public int Armadura { get => armadura; set => armadura = value; }
+        private TRAZA Raza { get => raza; set => raza = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public DateTime Fechanacimiento { get => fechanacimiento; set => fechanacimiento = value; }
+        public int Edad { get => this.edad; set => this.edad = value; }
+        public int Salud { get => salud; set => salud = value; }
+
+        public int edad(DateTime fechanacimiento)
+        {
+
+        }
+	}
+}
---
+++ Juego_Rol/Personaje.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+enum TRAZA {Orco =1 , Humano = 2 , Loli = 3};
+
+public Personaje()
+{
+	//datos
+	//caracteristicas//
+	int velocidad;
+	int destreza;
+	int fuerza;
+	int nivel;
+	int armadura;
+
+	TRAZA raza;
+
+}
+
